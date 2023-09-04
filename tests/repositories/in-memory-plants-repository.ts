7a014a372f578e2934865586f6ab09398ad95a97
silva4dev <web4dev.lucas@outlook.com>
2023-09-04 09:56:47 -0300

    chore: move the repository in memory to the tests folder

---
+++ tests/repositories/in-memory-plants-repository.ts
@@ -0,0 +1,20 @@
+import { Plant } from "../../src/domain/entities/plant.ts";
+import { PlantsRepository } from "../../src/domain/repositories/plants-repository.ts";
+
+export class InMemoryPlantsRepository implements PlantsRepository {
+  public plants: Plant[] = [];
+
+  public async create(data: Plant): Promise<void> {
+    this.plants.push(data);
+  }
+
+  public async findMany(): Promise<Plant[]> {
+    return this.plants;
+  }
+
+  public async createMany(data: Plant[]): Promise<Plant[]> {
+    data.forEach((plant) => this.plants.push(plant));
+
+    return this.plants;
+  }
+}
