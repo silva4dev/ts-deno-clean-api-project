9cefdb25fcb0aa78234859c9317b68b489474682
silva4dev <web4dev.lucas@outlook.com>
2023-10-29 10:49:27 -0300

    feat: add unique entity base

---
+++ src/common/entities/unique-entity-id.ts
@@ -0,0 +1,15 @@
+export class UniqueEntityID {
+	private value: string;
+
+	public toString() {
+		return this.value;
+	}
+
+	public toValue() {
+		return this.value;
+	}
+
+	constructor(value?: string) {
+		this.value = value ?? crypto.randomUUID();
+	}
+}
