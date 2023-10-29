089ddfd6e0cd4e0593ed759aa80ac2c314165739
silva4dev <web4dev.lucas@outlook.com>
2023-10-29 13:16:54 -0300

    chore: add error handling with the either type

--- src/application/list-plants-command.ts
+++ src/application/list-plants-command.ts
@@ -1,15 +1,26 @@
 import { Plant } from '@/domain/entities/plant.ts'
 import { PlantsRepository } from '@/domain/repositories/plants-repository.ts'
 import { Command } from '@/common/interfaces/command.ts'
+import { Either, right } from '@/common/either.ts'
 
-export class ListPlantsCommand implements Command<void, Plant[]> {
+type ListPlantsCommandResponse = Either<
+	null,
+	{
+		plants: Plant[]
+	}
+>
+
+export class ListPlantsCommand
+	implements Command<void, ListPlantsCommandResponse> {
 	constructor(
 		private plantRepository: PlantsRepository,
 	) {}
 
-	async execute(): Promise<Plant[]> {
+	async execute(): Promise<ListPlantsCommandResponse> {
 		const plants = await this.plantRepository.findMany()
 
-		return plants
+		return right({
+			plants,
+		})
 	}
 }
