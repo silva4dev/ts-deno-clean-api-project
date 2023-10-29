480abaed84b3b80e5a8070d146074001d74c5c25
silva4dev <web4dev.lucas@outlook.com>
2023-10-29 19:12:29 -0300

    test: add plant entity

---
+++ src/domain/entities/plant.test.ts
@@ -0,0 +1,27 @@
+import { assertEquals } from 'https://deno.land/std@0.200.0/assert/mod.ts'
+import { describe, it } from 'https://deno.land/std@0.204.0/testing/bdd.ts'
+import { Plant } from './plant.ts'
+
+describe('Plant entity', () => {
+	it('Should create a Plant instance with the provided data', async () => {
+		const result = Plant.create({
+			name: 'Suculenta',
+			type: 'Suculenta',
+			description: 'A beautiful cactus',
+			careInstructions: [
+				'Water once a week',
+				'Keep in indirect light',
+			],
+			imageUrl: 'https://example.com/suculenta.jpg',
+		})
+
+		assertEquals(result.careInstructions.length, 2)
+		assertEquals(result.name, 'Suculenta')
+		assertEquals(result.type, 'Suculenta')
+		assertEquals(result.description, 'A beautiful cactus')
+		assertEquals(
+			result.imageUrl,
+			'https://example.com/suculenta.jpg',
+		)
+	})
+})
