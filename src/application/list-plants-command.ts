924d9d82107a7fd19e96462929708f421ce7520b
silva4dev <web4dev.lucas@outlook.com>
2023-10-29 10:47:36 -0300

    feat: add list plants command

---
+++ src/application/list-plants-command.ts
@@ -0,0 +1,15 @@
+import { Plant } from '@/domain/entities/plant.ts';
+import { PlantsRepository } from '@/domain/repositories/plants-repository.ts';
+import { Command } from '@/common/interfaces/command.ts';
+
+export class ListPlantsCommand implements Command<void, Plant[]> {
+	constructor(
+		private plantRepository: PlantsRepository,
+	) {}
+
+	async execute(): Promise<Plant[]> {
+		const plants = await this.plantRepository.findMany();
+
+		return plants;
+	}
+}
