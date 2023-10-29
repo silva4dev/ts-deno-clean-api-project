25f7f4d7b295b510552d0dc254c09cc165900f49
silva4dev <web4dev.lucas@outlook.com>
2023-10-29 10:51:51 -0300

    feat: add make create plant factory

---
+++ tests/factories/make-create-plant-factory.ts
@@ -0,0 +1,12 @@
+import { CreatePlantCommand } from '@/application/create-plant-command.ts';
+import { InMemoryPlantsRepository } from '../repositories/in-memory-plants-repository.ts';
+
+export function makeCreatePlantFactory() {
+	const inMemoryPlantsRepository = new InMemoryPlantsRepository();
+	const createPlantCommand = new CreatePlantCommand(inMemoryPlantsRepository);
+
+	return {
+		inMemoryPlantsRepository,
+		createPlantCommand,
+	};
+}
