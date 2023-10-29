be4cf5f3003fc67f2c5aa0fdd0af0eb7e7c45c26
silva4dev <web4dev.lucas@outlook.com>
2023-10-29 10:49:17 -0300

    feat: add entity base

---
+++ src/common/entities/entity.ts
@@ -0,0 +1,15 @@
+import { UniqueEntityID } from './unique-entity-id.ts';
+
+export class Entity<Props> {
+	private _id: UniqueEntityID;
+	protected props: Props;
+
+	public get id() {
+		return this._id;
+	}
+
+	protected constructor(props: Props, id?: UniqueEntityID) {
+		this.props = props;
+		this._id = id ?? new UniqueEntityID();
+	}
+}
