9f583ec0ec0f0d024b81676965d6604dab69816a
silva4dev <web4dev.lucas@outlook.com>
2023-10-29 13:16:16 -0300

    test: add error handling with the either type

--- src/application/create-plant-command.test.ts
+++ src/application/create-plant-command.test.ts
@@ -1,7 +1,4 @@
-import {
-	assertArrayIncludes,
-	assertEquals,
-} from 'https://deno.land/std@0.200.0/assert/mod.ts'
+import { assertEquals } from 'https://deno.land/std@0.200.0/assert/mod.ts'
 import { describe, it } from 'https://deno.land/std@0.204.0/testing/bdd.ts'
 import { makeCreatePlantFactory } from '../../tests/factories/make-create-plant-factory.ts'
 
@@ -9,7 +6,7 @@ describe('Create Plant Command', () => {
 	it('Should create a new plant', async () => {
 		const { createPlantCommand } = makeCreatePlantFactory()
 
-		const plant = await createPlantCommand.execute({
+		const result = await createPlantCommand.execute({
 			name: 'Suculenta',
 			type: 'Suculenta',
 			description: 'A beautiful cactus',
@@ -20,13 +17,13 @@ describe('Create Plant Command', () => {
 			imageUrl: 'https://example.com/suculenta.jpg',
 		})
 
-		assertEquals(plant.name, 'Suculenta')
-		assertEquals(plant.type, 'Suculenta')
-		assertEquals(plant.description, 'A beautiful cactus')
-		assertEquals(plant.imageUrl, 'https://example.com/suculenta.jpg')
-		assertArrayIncludes(plant.careInstructions, [
-			'Water once a week',
-			'Keep in indirect light',
-		])
+		assertEquals(result.value?.plant.careInstructions.length, 2)
+		assertEquals(result.value?.plant.name, 'Suculenta')
+		assertEquals(result.value?.plant.type, 'Suculenta')
+		assertEquals(result.value?.plant.description, 'A beautiful cactus')
+		assertEquals(
+			result.value?.plant.imageUrl,
+			'https://example.com/suculenta.jpg',
+		)
 	})
 })
