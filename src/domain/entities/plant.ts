6d33d00797809e173a3ed8d7eb8f7317bab71252
silva4dev <web4dev.lucas@outlook.com>
2023-10-29 10:50:54 -0300

    chore: rename imports

--- src/domain/entities/plant.ts
+++ src/domain/entities/plant.ts
@@ -1,55 +1,55 @@
-import { Optional } from "../../domain/helpers/types/optional.ts";
-import { Entity } from "./entity.ts";
-import { UniqueEntityID } from "./unique-entity-id.ts";
+import { Entity } from '@/common/entities/entity.ts';
+import { UniqueEntityID } from '@/common/entities/unique-entity-id.ts';
+import { Optional } from '@/common/types/optional.ts';
 
 interface PlantProps {
-  name: string;
-  type: string;
-  description: string;
-  careInstructions: string[];
-  imageUrl: string;
-  createdAt: Date;
-  updatedAt?: Date;
+	name: string;
+	type: string;
+	description: string;
+	careInstructions: string[];
+	imageUrl: string;
+	createdAt: Date;
+	updatedAt?: Date;
 }
 
 export class Plant extends Entity<PlantProps> {
-  public get name() {
-    return this.props.name;
-  }
+	public get name() {
+		return this.props.name;
+	}
 
-  public get type() {
-    return this.props.type;
-  }
+	public get type() {
+		return this.props.type;
+	}
 
-  public get description() {
-    return this.props.description;
-  }
+	public get description() {
+		return this.props.description;
+	}
 
-  public get careInstructions() {
-    return this.props.careInstructions;
-  }
+	public get careInstructions() {
+		return this.props.careInstructions;
+	}
 
-  public get imageUrl() {
-    return this.props.imageUrl;
-  }
+	public get imageUrl() {
+		return this.props.imageUrl;
+	}
 
-  public get createdAt() {
-    return this.props.createdAt;
-  }
+	public get createdAt() {
+		return this.props.createdAt;
+	}
 
-  public get updatedAt() {
-    return this.props.updatedAt;
-  }
+	public get updatedAt() {
+		return this.props.updatedAt;
+	}
 
-  public static create(
-    props: Optional<PlantProps, "createdAt">,
-    id?: UniqueEntityID,
-  ) {
-    const plant = new Plant({
-      ...props,
-      createdAt: new Date(),
-    }, id);
+	public static create(
+		props: Optional<PlantProps, 'createdAt'>,
+		id?: UniqueEntityID,
+	) {
+		const plant = new Plant({
+			...props,
+			createdAt: new Date(),
+		}, id);
 
-    return plant;
-  }
+		return plant;
+	}
 }
