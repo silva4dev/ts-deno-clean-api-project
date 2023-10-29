ddc42526f81dc4f860fd5973e2be2d392d1ebeb8
silva4dev <web4dev.lucas@outlook.com>
2023-10-29 19:13:28 -0300

    chore: changing make create plant factory

--- tests/factories/make-create-plant-factory.ts
+++ tests/factories/make-create-plant-factory.ts
@@ -1,12 +1,25 @@
-import { CreatePlantCommand } from '@/application/create-plant-command.ts'
-import { InMemoryPlantsRepository } from '../repositories/in-memory-plants-repository.ts'
+import { Plant, PlantProps } from '@/domain/entities/plant.ts'
+import { UniqueEntityID } from '@/common/entities/unique-entity-id.ts'
+import { faker } from 'https://deno.land/x/deno_faker@v1.0.3/mod.ts'
 
-export function makeCreatePlantFactory() {
-	const inMemoryPlantsRepository = new InMemoryPlantsRepository()
-	const createPlantCommand = new CreatePlantCommand(inMemoryPlantsRepository)
+export function makeCreatePlantFactory(
+	override: Partial<PlantProps> = {},
+	id?: UniqueEntityID,
+) {
+	const plant = Plant.create(
+		{
+			name: faker.name.title(),
+			type: faker.name.jobType(),
+			description: faker.name.jobDescriptor(),
+			careInstructions: [
+				faker.name.jobTitle(),
+				faker.name.jobTitle(),
+			],
+			imageUrl: faker.internet.url(),
+			...override,
+		},
+		id,
+	)
 
-	return {
-		inMemoryPlantsRepository,
-		createPlantCommand,
-	}
+	return plant
 }
