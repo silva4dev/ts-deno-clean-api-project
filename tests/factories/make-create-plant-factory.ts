4c7246c77d449b64784175b506c8cf6defe12f86
silva4dev <web4dev.lucas@outlook.com>
2023-11-02 19:06:51 -0300

    chore: add generate random care instructions in create plant factory

--- tests/factories/make-create-plant-factory.ts
+++ tests/factories/make-create-plant-factory.ts
@@ -6,15 +6,23 @@ export function makeCreatePlantFactory(
 	override: Partial<PlantProps> = {},
 	id?: UniqueEntityID,
 ) {
+	const generateRandomCareInstructions = () => {
+		const numInstructions = Math.floor(Math.random() * 10) + 1
+		const careInstructions = []
+
+		for (let i = 0; i < numInstructions; i++) {
+			careInstructions.push(faker.name.jobTitle())
+		}
+
+		return careInstructions
+	}
+
 	const plant = Plant.create(
 		{
 			name: faker.name.title(),
 			type: faker.name.jobType(),
 			description: faker.name.jobDescriptor(),
-			careInstructions: [
-				faker.name.jobTitle(),
-				faker.name.jobTitle(),
-			],
+			careInstructions: generateRandomCareInstructions(),
 			imageUrl: faker.internet.url(),
 			...override,
 		},
