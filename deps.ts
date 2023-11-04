ae9c022c05e962d39d12409b16cedb945d9e5ca3
silva4dev <web4dev.lucas@outlook.com>
2023-11-04 17:05:46 -0300

    feat: add dependencies

---
+++ deps.ts
@@ -0,0 +1,7 @@
+export { assertEquals } from 'https://deno.land/std@0.200.0/assert/mod.ts'
+export { faker } from 'https://deno.land/x/deno_faker@v1.0.3/mod.ts'
+export {
+	beforeEach,
+	describe,
+	it,
+} from 'https://deno.land/std@0.204.0/testing/bdd.ts'
